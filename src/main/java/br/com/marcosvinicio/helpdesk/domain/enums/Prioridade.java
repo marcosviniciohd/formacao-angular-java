0b5ccaaa89acfd89c18913b2906dd1a23cc5afee
Marcos Vinício Fernandes <marcosviniciohd@hotmail.com>
2023-01-12 16:05:31 -0300

    Tipos enumerados criados

---
+++ src/main/java/br/com/marcosvinicio/helpdesk/domain/enums/Prioridade.java
@@ -0,0 +1,35 @@
+package br.com.marcosvinicio.helpdesk.domain.enums;
+
+public enum Prioridade {
+    BAIXA(0, "BAIXA"),
+    MEDIA(1, "MEDIA"),
+    ALTA(2, "ALTA");
+
+    private Integer codigo;
+    private String descricao;
+
+    Prioridade(Integer codigo, String descricao) {
+        this.codigo = codigo;
+        this.descricao = descricao;
+    }
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public static Prioridade toEnum(Integer cod) throws IllegalAccessException {
+        if (cod == null){
+            return null;
+        }
+        for (Prioridade x : Prioridade.values()){
+            if (cod.equals(x.getCodigo())){
+                return x;
+            }
+        }
+        throw new IllegalAccessException("Prioridade inválido");
+    }
+}
---
+++ src/main/java/br/com/marcosvinicio/helpdesk/domain/enums/Status.java
@@ -0,0 +1,35 @@
+package br.com.marcosvinicio.helpdesk.domain.enums;
+
+public enum Status {
+    ABERTO(0, "ABERTO"),
+    ANDAMENTO(1, "ANDAMENTO"),
+    ENCERRADO(2, "ENCERRADO");
+
+    private Integer codigo;
+    private String descricao;
+
+    Status(Integer codigo, String descricao) {
+        this.codigo = codigo;
+        this.descricao = descricao;
+    }
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public static Status toEnum(Integer cod) throws IllegalAccessException {
+        if (cod == null){
+            return null;
+        }
+        for (Status x : Status.values()){
+            if (cod.equals(x.getCodigo())){
+                return x;
+            }
+        }
+        throw new IllegalAccessException("Status inválido");
+    }
+}
